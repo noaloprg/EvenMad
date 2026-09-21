f9ddd1ee6832096854e04c6dee18a2dd6af1b763
Noa <noaloprg@gmail.com>
2026-09-21 23:00:38 +0200

    feat(VenueCreateDTO): crea DTO de creacion de venue

---
+++ backend/src/main/java/lopez/noa/evenMad/DTO/venue/VenueCreateDTO.java
@@ -0,0 +1,15 @@
+package lopez.noa.evenMad.DTO.venue;
+
+import lopez.noa.evenMad.documents.subdocuments.Address;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+
+import java.util.List;
+
+public record VenueCreateDTO(
+        String name,
+        int capacity,
+        List<String> affectedAreas,
+        GeoJsonPoint location,
+        Address address
+) {
+}
